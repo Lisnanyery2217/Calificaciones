93ec764c8880eff1a975674bae11c10363a0274d
lisnanyery <ruizlisnanyery@gmail.com>
2026-01-29 19:59:49 -0500

    calificaciones

---
+++ Calificaciones/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace Calificaciones
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtn4 = new System.Windows.Forms.TextBox();
+            this.txtn3 = new System.Windows.Forms.TextBox();
+            this.txtn2 = new System.Windows.Forms.TextBox();
+            this.txtn1 = new System.Windows.Forms.TextBox();
+            this.txtp = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonc = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.txte = new System.Windows.Forms.TextBox();
+            this.txtf = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calificacion final";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "promedio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(143, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nota extraordinario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(122, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Nota completivo";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(36, 275);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Nota 4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(36, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Nota 3";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(36, 149);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Nota 2";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(36, 88);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(56, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Nota 1";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(36, 38);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(131, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Ingrese las notas";
+            // 
+            // txtn4
+            // 
+            this.txtn4.Location = new System.Drawing.Point(98, 275);
+            this.txtn4.Name = "txtn4";
+            this.txtn4.Size = new System.Drawing.Size(100, 26);
+            this.txtn4.TabIndex = 9;
+            // 
+            // txtn3
+            // 
+            this.txtn3.Location = new System.Drawing.Point(98, 206);
+            this.txtn3.Name = "txtn3";
+            this.txtn3.Size = new System.Drawing.Size(100, 26);
+            this.txtn3.TabIndex = 10;
+            // 
+            // txtn2
+            // 
+            this.txtn2.Location = new System.Drawing.Point(98, 146);
+            this.txtn2.Name = "txtn2";
+            this.txtn2.Size = new System.Drawing.Size(100, 26);
+            this.txtn2.TabIndex = 11;
+            // 
+            // txtn1
+            // 
+            this.txtn1.Location = new System.Drawing.Point(98, 85);
+            this.txtn1.Name = "txtn1";
+            this.txtn1.Size = new System.Drawing.Size(100, 26);
+            this.txtn1.TabIndex = 12;
+            // 
+            // txtp
+            // 
+            this.txtp.Location = new System.Drawing.Point(87, 36);
+            this.txtp.Name = "txtp";
+            this.txtp.Size = new System.Drawing.Size(100, 26);
+            this.txtp.TabIndex = 13;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtp);
+            this.groupBox1.Location = new System.Drawing.Point(290, 312);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(199, 80);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "promedio";
+            // 
+            // buttonc
+            // 
+            this.buttonc.Location = new System.Drawing.Point(341, 398);
+            this.buttonc.Name = "buttonc";
+            this.buttonc.Size = new System.Drawing.Size(82, 29);
+            this.buttonc.TabIndex = 15;
+            this.buttonc.Text = "Calcular";
+            this.buttonc.UseVisualStyleBackColor = true;
+            this.buttonc.Click += new System.EventHandler(this.buttonc_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtf);
+            this.groupBox2.Controls.Add(this.txte);
+            this.groupBox2.Controls.Add(this.txtc);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(275, 25);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(264, 207);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Resultado";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(141, 37);
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(100, 26);
+            this.txtc.TabIndex = 4;
+            // 
+            // txte
+            // 
+            this.txte.Location = new System.Drawing.Point(158, 84);
+            this.txte.Name = "txte";
+            this.txte.Size = new System.Drawing.Size(100, 26);
+            this.txte.TabIndex = 5;
+            // 
+            // txtf
+            // 
+            this.txtf.Location = new System.Drawing.Point(141, 127);
+            this.txtf.Name = "txtf";
+            this.txtf.Size = new System.Drawing.Size(100, 26);
+            this.txtf.TabIndex = 17;
+            this.txtf.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.buttonc);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtn1);
+            this.Controls.Add(this.txtn2);
+            this.Controls.Add(this.txtn3);
+            this.Controls.Add(this.txtn4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtn4;
+        private System.Windows.Forms.TextBox txtn3;
+        private System.Windows.Forms.TextBox txtn2;
+        private System.Windows.Forms.TextBox txtn1;
+        private System.Windows.Forms.TextBox txtp;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonc;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txte;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.TextBox txtf;
+    }
+}
+
